94fde7ca8cdca8b3836e67885df772a57625c78d
Hyunju Woo <44936075+DeveloperWoo@users.noreply.github.com>
2019-02-06 21:33:23 -0500

    Add files via upload

--- chapter2_exercise.py
+++ chapter2_exercise.py
@@ -1,8 +1,10 @@
 #Chapter 2
 
-#Exercise 2: Write a program that uses input to prompt a user for their name and then welcomes them
-#Enter your name: Chuck
-#Hello Chuck
+'''
+Exercise 2: Write a program that uses input to prompt a user for their name and then welcomes them
+Enter your name: Chuck
+Hello Chuck
+'''
 print("<Exercise 2>")
 
 name = input('Enter your name: ')
@@ -10,11 +12,13 @@ print('Hello', name)
 
 print()
 
-#Exercise 3: Write a program to prompt the user for hours and rate per hour to compute gross pay.
-#Enter Hours: 35
-#Enter Rate: 2.75
-#Pay: 96.25
+'''
+Exercise 3: Write a program to prompt the user for hours and rate per hour to compute gross pay.
+Enter Hours: 35
+Enter Rate: 2.75
+Pay: 96.25
 print("<Exercise 3>")
+'''
 
 hours = float(input("Enter Hours: "))
 rate = float(input("Enter rate: "))
@@ -23,9 +27,11 @@ print("Pay: ", pay)
 
 print()
 
-#Exercise 4: Assume that we execute the following assignment statements:
+'''
+Exercise 4: Assume that we execute the following assignment statements:
 width = 17
 height = 12.0
+'''
 print("<Exercise 4>")
 
 print(width/2) #8.5
@@ -39,10 +45,11 @@ print(type(1+2*5)) #<class 'int'>
 
 print()
 
-#Exercise 5: Write a program which prompts the user for a Celsius temperature,
-#convert the temperature to Fahrenheit, and print out the converted temperature.
-#formula: (0°C × 9/5) + 32 = 32°F
-
+'''
+Exercise 5: Write a program which prompts the user for a Celsius temperature,
+convert the temperature to Fahrenheit, and print out the converted temperature.
+*formula: (0°C × 9/5) + 32 = 32°F
+'''
 print("<Exercise 5>")
 
 tempInCelsius = float(input("Enter a Celsius temperature: "))
