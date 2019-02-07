c3c9380e469f821cc748c03d3e8a2283301be9c4
Hyunju Woo <44936075+DeveloperWoo@users.noreply.github.com>
2019-02-06 21:33:33 -0500

    Add files via upload

--- chapter3_exercise.py
+++ chapter3_exercise.py
@@ -1,9 +1,10 @@
-#Exercise 1: Write your pay computation to give the employee
-#1.5times the hourly rate for hours worked above 40 hours.
-
-#Ent#er Hours: 45
-#Enter Rate: 10
-#Pay: 475.0
+'''
+Exercise 1: Write your pay computation to give the employee
+1.5times the hourly rate for hours worked above 40 hours.
+Ent#er Hours: 45
+Enter Rate: 10
+Pay: 475.0
+'''
 print("<Exercise 1>")
 
 hrs = input("Enter Hours:")
@@ -19,15 +20,17 @@ else:
 print("Pay:", pay)
 
 
-#Exercise 2: Rewrite your pay program using try and except so that your
-#program handles non-numeric input gracefully by printing a message
-#and exiting the program. The following shows two executions of the program:
+'''
+Exercise 2: Rewrite your pay program using try and except so that your
+program handles non-numeric input gracefully by printing a message
+and exiting the program. The following shows two executions of the program:
+Enter Hours: 20
+Enter Rate: nine
+Error, please enter numeric input
+Enter Hours: forty
+Error, please enter numeric input
+'''
 
-#Enter Hours: 20
-#Enter Rate: nine
-#Error, please enter numeric input
-#Enter Hours: forty
-#Error, please enter numeric input
 print("<Exercise 2>")
 
 hrs2 = input("Enter Hours:")
